0cfb39e7ab3544bf0f734f9352c56366c7d16af4
Segundobenito <sbenito@udesa.edu.ar>
2023-04-01 18:31:53 -0300

    footer

--- proyecto-integrador/src/components/Footer/Footer.js
+++ proyecto-integrador/src/components/Footer/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import './styles.css';
 
  function Footer() {
     return (
         <footer>
-            
+            <div className = "footer-content">
+                <h3>MyFlixer</h3>
+                <p>Copyright</p>
+            </div>
+
+            <ul className = "names">
+                <li><a href>Segundo Benito</a></li>
+                <li><a href>Zackarias O'Farrell</a></li>
+                <li><a href>Tomas Arizu</a></li>
+            </ul>
+
+            <div className = "footer-bottom">
+                <p>copyright &copy; 2023 MyFlixer. designed by <span>udesa</span></p>
+            </div>
+
         </footer>
     )
 }
