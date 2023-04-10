2be6e656b3fe9c128e1441e09b08fae865414c39
tomasarizu <tarizu@udesa.edu.ar>
2023-04-10 12:05:53 -0300

    serieDetalle

--- proyecto-integrador/src/screens/serieDetalle/serieDetalle.js
+++ proyecto-integrador/src/screens/serieDetalle/serieDetalle.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
- class CancionDetalle extends Component {
+ class SeriesDetalle extends Component {
      constructor(props){
          super(props)
          this.state = {
               id: props.match.params.id,
-              pelicula: {},
+              series: {},
               generos: []
          }
          
@@ -13,10 +13,10 @@ import React, { Component } from 'react'
 
      componentDidMount() {
         
-        fetch(`https://api.themoviedb.org/3/movie/${this.state.id}?api_key=8fe477c9339c0a2e32b6b0fce8059603 ` )
+        fetch(`https://api.themoviedb.org/3/tv/${this.state.id}?api_key=8fe477c9339c0a2e32b6b0fce8059603 ` )
         .then(res => res.json())
         .then(data => this.setState({
-            pelicula: data,
+            series: data,
             generos: data.genres,
         }))
         .catch(err => console.log(err))
@@ -32,16 +32,15 @@ import React, { Component } from 'react'
                 
                 {
                     
-                    this.state.pelicula.title === 0 ?
+                    this.state.series.title === 0 ?
                     <p>No tenemos lo que buscas</p> 
                     :
                     <div>
-                        <h2>{this.state.pelicula.title}</h2>
-                        <img src={`https://image.tmdb.org/t/p/w342/${this.state.pelicula.poster_path}`}/>
-                        <p>Rating: {this.state.pelicula.popularity}</p>
-                        <p>Fecha de Estreno: {this.state.pelicula.release_date}</p>
-                        <p>Duracion: {this.state.pelicula.runtime}</p>
-                        <p>Sinopsis: {this.state.pelicula.overview}</p>
+                        <h2>{this.state.series.name}</h2>
+                        <img src={`https://image.tmdb.org/t/p/w342/${this.state.series.poster_path}`}/>
+                        <p>Rating: {this.state.series.popularity}</p>
+                        <p>Fecha de Estreno: {this.state.series.first_air_date}</p>
+                        <p>Sinopsis: {this.state.series.overview}</p>
                         <ul>Generos:{this.state.generos.map((genero, idx) => <li key = {genero.name + idx}>{genero.name}</li>)}</ul>
                        
 
@@ -55,4 +54,4 @@ import React, { Component } from 'react'
     }
 }
 
-export default CancionDetalle;
+export default SeriesDetalle;
