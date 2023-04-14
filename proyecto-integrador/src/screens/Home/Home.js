17c16a8484bcd998b1c91295f2ac563c65303702
tomasarizu <tarizu@udesa.edu.ar>
2023-04-14 19:48:24 -0300

    agregue cosas

--- proyecto-integrador/src/screens/Home/Home.js
+++ proyecto-integrador/src/screens/Home/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ContenedorHome from '../../components/ContenedorHome/ContenedorHome';
 import ContenedorHomeSeries from '../../components/ContenedorHomeSeries/ContenedorHomeSeries';
-import Buscador from '../../components/Buscador/Buscador';
 import './styles.css';
 
 
 function Home() {
     return (
         <>
-            <Buscador />
             <h1> PELICULAS</h1>
             <ContenedorHome />
             <h1 className='seriesH1'> SERIES</h1>
